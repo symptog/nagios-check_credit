753800d371aae3e782d2714c8ec73c160875607d
Tobias Gall <symptog@gmail.com>
2015-01-29 00:03:01 +0100

    Initial

---
+++ GSM1.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+
+[ -z "$DEVICENAME" ] && tmp=${0##*/} && DEVICENAME=${tmp%%.*}
+balance_file="/var/log/smstools/smsd_stats/${DEVICENAME}.balance"
+regular_run_cmdfile="/opt/sms/regular_run/${DEVICENAME}.cmdfile"
+regular_run_statfile="$2"
+
+###########################################################################
+# START OF OPERATOR SPECIFIC SETTINGS
+
+# If more than one modem is used and they have different operator specific
+# settings, move this section to the files for each modem and include the#!/bin/bash
+
+
+[ -z "$DEVICENAME" ] && tmp=${0##*/} && DEVICENAME=${tmp%%.*}
+balance_file="/var/log/smstools/smsd_stats/${DEVICENAME}.balance"
+regular_run_cmdfile="/opt/sms/regular_run/${DEVICENAME}.cmdfile"
+regular_run_statfile="$2"
+
+###########################################################################
+# START OF OPERATOR SPECIFIC SETTINGS
+
+# If more than one modem is used and they have different operator specific
+# settings, move this section to the files for each modem and include the
+# file here using the following command:
+# . /var/spool/sms/regular_run/${DEVICENAME}.operator_settings.sh
+
+# Fix this if a different command is required in your network:
+#ussd_command="AT+CPMS=\"SM\",\"*100#\""
+ussd_command="AT+CUSD=1,\"*100#\""
+
+if [ "$1" = "PRE_RUN" ]; then
+	echo "$ussd_command" > "$regular_run_cmdfile"
+else
+
+	result=""
+	if [ -r "$regular_run_statfile" ]; then
+		tmp=${ussd_command//\"/\\\"}
+		tmp=${tmp//\*/\\*}
+		result=$(cat "$regular_run_statfile" | grep "$tmp")
+	fi
+
+	echo "$result" > "$balance_file"
+fi
+
+exit 0
+
+# file here using the following command:
+# . /var/spool/sms/regular_run/${DEVICENAME}.operator_settings.sh
+
+# Fix this if a different command is required in your network:
+#ussd_command="AT+CPMS=\"SM\",\"*100#\""
+ussd_command="AT+CUSD=1,\"*100#\""
+
+if [ "$1" = "PRE_RUN" ]; then
+	echo "$ussd_command" > "$regular_run_cmdfile"
+else
+
+	result=""
+	if [ -r "$regular_run_statfile" ]; then
+		tmp=${ussd_command//\"/\\\"}
+		tmp=${tmp//\*/\\*}
+		result=$(cat "$regular_run_statfile" | grep "$tmp")
+	fi
+
+	echo "$result" > "$balance_file"
+fi
+
+exit 0
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # nagios-check_credit
-Nagios Plugin - Check Prepaid Credit
+Nagios/Icinga Plugin - Check Prepaid Credit using SMSTools3
+
+Based on http://smstools3.kekekasvi.com/topic.php?id=320
+
+## Install
+
+Copy `GMS1.sh`
+```
+cp GSM1.sh /opt/sms/regular_run/GSM1.sh
+chown -R smsd:smsd /opt/sms
+```
+
+Append to `/etc/smsd.conf`
+```
+regular_run = /opt/sms/regular_run/GSM1.sh
+regular_run_interval = 43200
+regular_run_post_run = /opt/sms/regular_run/GSM1.sh
+regular_run_cmdfile = /opt/sms/regular_run/GSM1.cmdfile
+regular_run_statfile = /opt/sms/regular_run/GSM1.statfile
+```
+
+Copy `check_credit` to your Nagios/Icinga Plugins
+
+Append to Nagios/Icinga Commandfile
+```
+define command{
+        command_name    check_credit
+        command_line    $USER1$/check_balance -d GSM1 -w 2.0 -c 1.0 -currency EUR -s /etc/smsd.conf
+}
+```
+
+all paramters are optional
+
+## Help
+usage: check_balance [-h] [--verbose] [-c CRITICAL] [-w WARNING] [-s SMSCONF]
+                     [-d DEVICE] [-currency CURRENCY]
+
+Prepaid balance check for Nagios
+
+optional arguments:
+  -h, --help          show this help message and exit
+  --verbose           verbose flag
+  -c CRITICAL         Critical value eg. 1.0
+  -w WARNING          Warning value eg 2.0
+  -s SMSCONF          SMSTools3 Config Path eg. /etc/smsd.conf
+  -d DEVICE           Device eg. GSM1
+  -currency CURRENCY  Currency eg. EUR
