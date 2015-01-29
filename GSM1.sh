a5ef5c67ad6b099191485a5c7b21b6cf99a70e83
Tobias Gall <symptog@gmail.com>
2015-01-29 10:20:15 +0100

    Update GSM1.sh

--- GSM1.sh
+++ GSM1.sh
@@ -11,17 +11,7 @@ balance_file="/var/log/smstools/smsd_stats/${DEVICENAME}.balance"
 regular_run_cmdfile="/opt/sms/regular_run/${DEVICENAME}.cmdfile"
 regular_run_statfile="$2"
 
-###########################################################################
-# START OF OPERATOR SPECIFIC SETTINGS
-
-# If more than one modem is used and they have different operator specific
-# settings, move this section to the files for each modem and include the
-# file here using the following command:
-# . /var/spool/sms/regular_run/${DEVICENAME}.operator_settings.sh
-
-
 # Fix this if a different command is required in your network:
-#ussd_command="AT+CPMS=\"SM\",\"*100#\""
 ussd_command="AT+CUSD=1,\"*100#\""
 
 if [ "$1" = "PRE_RUN" ]; then
